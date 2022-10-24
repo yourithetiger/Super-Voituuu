7e2481bcc494b6c484a95726dc7c0c138008b802
m2kdev <91156230+m2kdev@users.noreply.github.com>
2022-10-24 19:58:05 +0200

    modif

--- projetnsi.py
+++ projetnsi.py
@@ -9,9 +9,13 @@ gray=(60,60,60)
 black=(255,0,0)
 
 fenetre=display.set_mode((640,480))
-display.set_caption("voituuu")
-sol = image.load("backstart.jpg").convert()
-fenetre.blit(sol, (0,0))
+display.set_caption("voituu")
+route = image.load("backstart.jpg").convert()
+fenetre.blit(route, (0,0))
+
+voituu = pygame.image.load("voituu").convert_alpha()
+fenetre.blit(voituu, (250,300))
+
 
 play=1
 while play:
@@ -19,7 +23,3 @@ while play:
     for event in event.get():
         if event.type == QUIT:
             play = 0
-            
-    
-
-
