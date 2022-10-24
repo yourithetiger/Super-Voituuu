48e18a49d6e59ac003a018674a89ce7aa435ad72
yourithetiger <66949903+yourithetiger@users.noreply.github.com>
2022-10-24 22:26:02 +0200

    deplacement fond+voiture

--- projetnsi.py
+++ projetnsi.py
@@ -1,40 +1,44 @@
 from pygame import *
-import pygame
+import pygame #
 import time
 import random
 
-
 init()
+clock = pygame.time.Clock()
+fenetre_x = 1440
+fenetre_y = 864
+fenetre=display.set_mode((fenetre_x,fenetre_y))
+display.set_caption("voituuu")
 
-gray=(60,60,60)
-black=(255,0,0)
+#fond
+fond = image.load("backstart.jpg").convert()
+fenetre.blit(fond, (0,0))
+fond_y = 0
 
-fenetre=display.set_mode((640,480))
-route = image.load("backstart.jpg").convert()
-fenetre.blit(route, (0,0))
-
-
-voituu = pygame.image.load("voituu.png").convert_alpha()
-position_voituu = voituu.get_rect()
-fenetre.blit(voituu, (300, 300))
-voituu = pygame.transform.scale(voituu, (55, 100))
-
-
+#voiture
+car = image.load("voituuu.png").convert_alpha()
+car_pos =  car.get_rect()
+fenetre.blit(car, car_pos)
 display.flip()
-
+key.set_repeat(50, 2)
 play=1
 while play:
+
     for event in pygame.event.get():
         if event.type == QUIT:
             play = 0
-
         if event.type == KEYDOWN:
-                if event.key == K_q:
-                    position_voituu = position_voituu.move(-10,0)
-                if event.key == K_d:
-                    position_voituu = position_voituu.move(10,0)
+            if event.key == K_q:
+                    car_pos = car_pos.move(-1,0)
+            if event.key == K_d:
+                    car_pos = car_pos.move(1,0)
 
-    fenetre.blit(route, (0,0))
-    fenetre.blit(voituu, position_voituu)
+    clock.tick(60)
+    fond_y += 1
+    if fond_y > 864:
+        fond_y = 0
+    fenetre.blit(fond, (0, fond_y))
+    fenetre.blit(fond, (0, fond_y - 864))
+    fenetre.blit(car, car_pos)
+    display.flip()
 
-    pygame.display.flip()
