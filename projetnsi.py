8423d7bd49f805d118f0bc4cf0e4aa78e4594d9e
m2kdev <91156230+m2kdev@users.noreply.github.com>
2022-10-24 22:05:21 +0200

    voituu

--- projetnsi.py
+++ projetnsi.py
@@ -1,4 +1,5 @@
 from pygame import *
+import pygame
 import time
 import random
 
@@ -9,17 +10,31 @@ gray=(60,60,60)
 black=(255,0,0)
 
 fenetre=display.set_mode((640,480))
-display.set_caption("voituu")
 route = image.load("backstart.jpg").convert()
 fenetre.blit(route, (0,0))
 
-voituu = pygame.image.load("voituu").convert_alpha()
-fenetre.blit(voituu, (250,300))
 
+voituu = pygame.image.load("voituu.png").convert_alpha()
+position_voituu = voituu.get_rect()
+fenetre.blit(voituu, (300, 300))
+voituu = pygame.transform.scale(voituu, (55, 100))
+
+
+display.flip()
 
 play=1
 while play:
-    display.flip()
-    for event in event.get():
+    for event in pygame.event.get():
         if event.type == QUIT:
             play = 0
+
+        if event.type == KEYDOWN:
+                if event.key == K_q:
+                    position_voituu = position_voituu.move(-10,0)
+                if event.key == K_d:
+                    position_voituu = position_voituu.move(10,0)
+
+    fenetre.blit(route, (0,0))
+    fenetre.blit(voituu, position_voituu)
+
+    pygame.display.flip()
