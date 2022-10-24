128ff24798ded078d5412cd925365173b4fdd700
yourithetiger <66949903+yourithetiger@users.noreply.github.com>
2022-10-24 22:23:46 +0200

    background + deplacement voiture

--- projetnsi.py
+++ projetnsi.py
@@ -1,28 +1,44 @@
 from pygame import *
+import pygame #
 import time
 import random
 
-
 init()
-
-gray=(60,60,60)
-black=(255,0,0)
-
-fenetre=display.set_mode((640,480))
+clock = pygame.time.Clock()
+fenetre_x = 1440
+fenetre_y = 864
+fenetre=display.set_mode((fenetre_x,fenetre_y))
 display.set_caption("voituuu")
-sol = image.load("backstart.jpg").convert()
-fenetre.blit(sol, (0,0))
 
+#fond
+fond = image.load("backstart.jpg").convert()
+fenetre.blit(fond, (0,0))
+fond_y = 0
+
+#voiture
+car = image.load("voituuu.png").convert_alpha()
+car_pos =  car.get_rect()
+fenetre.blit(car, car_pos)
+display.flip()
+key.set_repeat(50, 2)
 play=1
 while play:
-    display.flip()
-    for event in event.get():
+
+    for event in pygame.event.get():
         if event.type == QUIT:
             play = 0
-            
-    
+        if event.type == KEYDOWN:
+            if event.key == K_q:
+                    car_pos = car_pos.move(-1,0)
+            if event.key == K_d:
+                    car_pos = car_pos.move(1,0)
 
-fenetre=display.set_mode((640,480))
-display.set_caption("voituuu")
-sol = image.load("backstart.jpg").convert()
-fenetre.blit(sol, (0,0))
+    clock.tick(60)
+    fond_y += 1
+    if fond_y > 864:
+        fond_y = 0
+    fenetre.blit(fond, (0, fond_y))
+    fenetre.blit(fond, (0, fond_y - 864))
+    fenetre.blit(car, car_pos)
+    display.flip()
+
