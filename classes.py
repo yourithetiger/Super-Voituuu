f72fcd058d11fcc6ab127a326080ccbb1d468581
yourithetiger <66949903+yourithetiger@users.noreply.github.com>
2022-10-26 00:54:34 +0200

    modif pour fonctionnement par classes

--- classes.py
+++ classes.py
@@ -2,10 +2,48 @@ from pygame import *
 from pygame.locals import * 
 from cons import *
 
-class Joueur:
-    def __init__(self, score):
-        self.x = 0
-        self.y = 0
-        self.score = 0
-    def deplacer(self, dir):
+class Game:
+    
+    def __init__(self):
+        self.player = Player()
+        self.background = image.load("fond.jpg").convert()
+        self.road = Road()
+        self.pressed = {}
+
+class Road:
+    
+    def __init__(self):
+        self.image = image.load("route.jpg").convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = 300
+        self.rect.y = 763
+        self.rect2 = self.rect
+        self.rect2.y -= 763
+    def defil(self):
+        self.rect = self.rect.move(0,1)
+        if self.rect.y > 763:
+            self.rect.y = 0
+        self.rect2.y = self.rect.y - 763
+        
+         
+class Player(sprite.Sprite):
+    def __init__(self) :
+         super().__init__()
+         self.score = 0
+         self.image = image.load("voituuu.png").convert_alpha()
+         self.image = transform.smoothscale(self.image, (100, 200))
+         self.rect = self.image.get_rect()
+         self.rect.x = 500
+         self.rect.y = 500
+         self.speed = 5
+
+    def move(self, dir):
+        if dir == 1 and not self.rect.x > 856:
+            self.rect = self.rect.move(dir*self.speed,0)
+        elif dir == -1 and not self.rect.x < 300:
+            self.rect = self.rect.move(dir*self.speed,0)
+
+        
+        
+        
         
--- projetnsi.py
+++ projetnsi.py
@@ -1,49 +1,49 @@
 from pygame import *
 import pygame # certaines fontions ne fonctionennent pas sans le "pygame."
 from cons import *
+from classes import *
 
 init()
+
+     
+
 clock = time.Clock()
 fenetre=display.set_mode((fenetre_x,fenetre_y))
 display.set_caption(titre)
 
-#fond
-fond = image.load("fond.jpg").convert()
-fenetre.blit(fond, (-400,0))
-#route
-route = image.load("route.jpg").convert()
-fenetre.blit(route, (300,0))
-route_y = 0
-
-#voiture
-car = image.load("voituuu.png").convert_alpha()
-car_pos =  car.get_rect()
-fenetre.blit(car, car_pos)
-car = pygame.transform.smoothscale(car, (100, 200))
 
 display.flip()
 
-key.set_repeat(50, 2)
-
+game = Game()
 play=1
 while play:
-
+    clock.tick(120)
+    fenetre.blit(game.background, (-400,0))
+    fenetre.blit(game.road.image, game.road.rect)
+    fenetre.blit(game.road.image, game.road.rect2)
+    fenetre.blit(game.player.image, game.player.rect)
+    game.road.defil()
+    
+    
+    if game.pressed.get(K_q):
+                 game.player.move(-1)
+    if game.pressed.get(K_d):
+                 game.player.move(1)
+                 
+                 
     for event in pygame.event.get():
+        
         if event.type == QUIT:
             play = 0
         if event.type == KEYDOWN:
-            if event.key == K_q:
-                    car_pos = car_pos.move(-1,0)
-            if event.key == K_d:
-                    car_pos = car_pos.move(1,0)
-
-    clock.tick(60)
-    route_y += 1
-    if route_y > 763:
-        route_y = 0
-    fenetre.blit(fond, (-400,0))
-    fenetre.blit(route, (300, route_y))
-    fenetre.blit(route, (300, route_y - 763))
-    fenetre.blit(car, car_pos)
+            game.pressed[event.key] = True
+        elif event.type == KEYUP:
+            game.pressed[event.key] = False
+            
     display.flip()
+
+
+
+
+
 
