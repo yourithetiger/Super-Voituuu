ea694266b4bab95cab5292be5e72e70808faeef1
Yemaboy <116841171+Yemaboy@users.noreply.github.com>
2022-10-29 19:05:01 +0200

    Update projetnsi.py

--- projetnsi.py
+++ projetnsi.py
@@ -1,24 +1,89 @@
 from pygame import *
-import time
-import random
-
+import pygame # certaines fontions ne fonctionennent pas sans le "pygame."
+from cons import *
+from classes import *
 
 init()
 
-gray=(60,60,60)
-black=(255,0,0)
+     
 
-fenetre=display.set_mode((640,480))
-display.set_caption("voituuu")
-sol = image.load("backstart.jpg").convert()
-fenetre.blit(sol, (0,0))
+clock = time.Clock()
+fenetre=display.set_mode((fenetre_x,fenetre_y))
+display.set_caption(titre)
 
+
+display.flip()
+
+game = Game()
 play=1
 while play:
-    display.flip()
-    for event in event.get():
+    clock.tick(120)
+    fenetre.blit(game.background, (-400,0))
+    fenetre.blit(game.road.image, game.road.rect)
+    fenetre.blit(game.road.image, game.road.rect2)
+    fenetre.blit(game.player.image, game.player.rect)
+    game.road.defil()
+    
+    
+    if game.pressed.get(K_q):
+                 game.player.move(-1)
+    if game.pressed.get(K_d):
+                 game.player.move(1)
+                 
+                 
+    for event in pygame.event.get():
+        
         if event.type == QUIT:
             play = 0
+        if event.type == KEYDOWN:
+            game.pressed[event.key] = True
+        elif event.type == KEYUP:
+            game.pressed[event.key] = False
+            
+    display.flip()
+    
+    
+    
+  background pygame.image.load('ressource/fond.jpg')
+
+background.convert()
+
+
+y_background = 1
+
+running = True
+
+
+
+running True
+
+while running:
+
+y_background +- 1
+
+
+
+if y_background < 619:
+
+screen.blit(background, (-109, y_background))
+
+screen.blit(background, (-109, y_background-619))
+
+else:
+
+y_background-e
+
+screen.blit(background, (-109, y_background))
+
+screen.blit(player.image, player.rect)
+
+clock.tick(60)
+print(f"{clock.get_fps()FPS }")
+
+pygame.display.flip()
+
+
+
             
     
 
