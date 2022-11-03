8f26e852c9ff7cac87ae3fbb8edff91bd1bde901
Yourithetiger <youri.ivanyhysn@gmail.com>
2022-11-04 00:59:16 +0100

    score git pull

--- projetnsi.py
+++ projetnsi.py
@@ -2,48 +2,51 @@ from pygame import *
 import pygame # certaines fontions ne fonctionennent pas sans le "pygame."
 from cons import *
 from classes import *
+import random
 
 init()
-
-     
-
 clock = time.Clock()
 fenetre=display.set_mode((fenetre_x,fenetre_y))
 display.set_caption(titre)
 
-
-display.flip()
-
-game = Game()
-play=1
-while play:
-    clock.tick(120)
-    fenetre.blit(game.background, (-400,0))
-    fenetre.blit(game.road.image, game.road.rect)
-    fenetre.blit(game.road.image, game.road.rect2)
-    fenetre.blit(game.player.image, game.player.rect)
-    game.road.defil()
-    
-    
-    if game.pressed.get(K_q):
-                 game.player.move(-1)
-    if game.pressed.get(K_d):
-                 game.player.move(1)
-                 
-                 
-    for event in pygame.event.get():
-        
+def main():
+  game = Game()
+  play=1
+  while play:
+      for event in pygame.event.get(): 
         if event.type == QUIT:
-            play = 0
-        if event.type == KEYDOWN:
-            game.pressed[event.key] = True
-        elif event.type == KEYUP:
-            game.pressed[event.key] = False
-            
-    display.flip()
+          play = 0         
+      clock.tick(120)
+      touche = pygame.key.get_pressed()
+      game.play(fenetre, touche) 
+      if game.dead:
+        menu(death_count=game.Deathcount)     
+      display.flip()
+      
+def menu(death_count):
+    global points
+    run = True
+
+    while run:
+        fenetre.fill((255, 255, 255))
 
 
+        if death_count == 0:
+            text = fonte.render("Press any Key to Start", True, Black)
+        elif death_count > 0:
+            text = fonte.render("Press any Key to Restart", True, Black)
+            score = fonte.render("bravo", True, Black)
+            scoreRect = score.get_rect()
+            scoreRect.center = (fenetre_x // 2, fenetre_y // 2 + 50)
+            fenetre.blit(score, scoreRect)
+        textRect = text.get_rect()
+        textRect.center = (fenetre_x // 2, fenetre_y // 2)
+        fenetre.blit(text, textRect)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                run = False
+            if event.type == KEYDOWN:
+                main()
+        display.flip()
 
-
-
-
+menu(death_count=0)
