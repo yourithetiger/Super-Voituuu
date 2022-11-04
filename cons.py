595157bb37bfa590869342f28a019f4ebda685e2
Yourithetiger <youri.ivanyhysn@gmail.com>
2022-11-05 00:51:31 +0100

    merge

---
+++ cons.py
@@ -0,0 +1,12 @@
+#fenetre
+from pygame import *
+init()
+multiplier = 1.5
+fenetre_x = 1050*multiplier
+fenetre_y = 763*multiplier
+titre = "Voituuu"
+GameSpeed = 5
+Black = (0,0,0)
+Red = (255,0,0)
+fonte = font.Font('freesansbold.ttf', 30)
+#images
---
+++ projetnsi.py
@@ -0,0 +1,51 @@
+from pygame import *
+import pygame # certaines fontions ne fonctionennent pas sans le "pygame."
+from cons import *
+from classes import *
+import random
+
+init()
+clock = time.Clock()
+fenetre=display.set_mode((fenetre_x,fenetre_y))
+display.set_caption(titre)
+
+def main():
+  game = Game()
+  play=1
+  while play:
+      for event in pygame.event.get(): 
+        if event.type == QUIT:
+          play = 0         
+      clock.tick(120)
+      touche = pygame.key.get_pressed()
+      game.play(fenetre, touche) 
+      if game.dead:
+        play = 0
+        menu(death_count=game.Deathcount)     
+      display.flip()
+      
+def menu(death_count):
+    run = 1
+
+    while run:
+        fenetre.fill((255, 255, 255))
+
+
+        if death_count == 0:
+            text = fonte.render("Press any Key to Start", True, Black)
+        elif death_count > 0:
+            text = fonte.render("Press any Key to Restart", True, Black)
+            score = fonte.render("bravo", True, Black)
+            scoreRect = score.get_rect()
+            scoreRect.center = (fenetre_x // 2, fenetre_y // 2 + 50)
+            fenetre.blit(score, scoreRect)
+        textRect = text.get_rect()
+        textRect.center = (fenetre_x // 2, fenetre_y // 2)
+        fenetre.blit(text, textRect)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                run = 0
+            if event.type == KEYDOWN:
+                main()
+        display.flip()
+menu(death_count=0)
